f1d25a6dcb9ef1439f9af47ed5d01f717ac83d39
SaihttamR <matthias@regtteor.de>
2023-01-18 15:58:47 +0100

    Removed dependency on user right "view_work_time_tab"

--- app/controllers/work_time_controller.rb
+++ app/controllers/work_time_controller.rb
@@ -867,7 +867,7 @@ private
     mem = Member.where(["project_id=:prj", {:prj=>@project.id}]).all
     mem_by_uid = {}
     mem.each do |m|
-      next if m.nil? || m.user.nil? || ! m.user.allowed_to?(:view_work_time_tab, @project)
+      next if m.nil? || m.user.nil?
       mem_by_uid[m.user_id] = m
     end
 
